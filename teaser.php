a13129b4e708d0bb2f7996bd4549380f386df01d
JulienVaglia <julien.vaglia@gmail.com>
2023-06-07 00:02:33 +0200

    avec_logo_et_texte

--- teaser.php
+++ teaser.php
@@ -13,20 +13,32 @@
 
     <main class="">
 
+        <a href="https://discord.gg/starisland" target="_blank" class=""><img id="logo" src="assets/img/Starisland_logo.png" alt="logo"></a>
+
+        <div class="texte">
+
+            <h1>Star'Island</h1>
+
+
+            <p class="presentation">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ullam, ad impedit explicabo rem possimus. Amet quia et id rerum veniam iure provident aliquam sunt molestiae deleniti eos obcaecati veritatis ab cupiditate expedita, nemo quidem aspernatur odit, aut reiciendis accusantium? Dignissimos nemo expedita voluptatum veritatis, inventore molestias perferendis alias consectetur?</p>
+
+
+        </div>
+
         <div id="timer"></div>
 
-<!-- Barre des liens -->
+        <!-- Barre des liens -->
 
         <div id="navLien">
 
             <div class="barre_liens">
-                <a href="" class="reseauSociaux" id="instagram"><img src="assets/img/logo Instagram.png" alt="instagram"></a>
-                <a href="" class="reseauSociaux" id="facebook"><img src="assets/img/logo facebook.png" alt="facebook"></a>
-                <a href="" class="reseauSociaux" id="tiktok"><img src="assets/img/logo tiktok.png" alt="tiktok"></a>               
-                <a href="" class="reseauSociaux" id="twitch"><img src="assets/img/logo twitch.png" alt="twitch"></a>
-                <a href="" class="reseauSociaux" id="twitter"><img src="assets/img/logo twitter.png" alt="twitter"></a>
-                <a href="" class="reseauSociaux" id="youtube"><img src="assets/img/logo youtube.png" alt="youtube"></a>
-                <a href=""  class="reseauSociaux discord"><img  src="assets/img/logo discord.png" alt="discord"></a>
+                <a href="https://www.instagram.com/starisland.fr/" target="_blank" class="reseauSociaux" id="instagram"><img src="assets/img/logo Instagram.png" alt="instagram"></a>
+                <a href="https://www.facebook.com/StarIslandfr-108004258577047" target="_blank" class="reseauSociaux" id="facebook"><img src="assets/img/logo facebook.png" alt="facebook"></a>
+                <a href="https://www.tiktok.com/@star.island?lang=fr" target="_blank" class="reseauSociaux" id="tiktok"><img src="assets/img/logo tiktok.png" alt="tiktok"></a>
+                <a href="" class="reseauSociaux" target="_blank" id="twitch"><img src="assets/img/logo twitch.png" alt="twitch"></a>
+                <a href="https://twitter.com/StarIslandfr" target="_blank" class="reseauSociaux" id="twitter"><img src="assets/img/logo twitter.png" alt="twitter"></a>
+                <a href="https://www.youtube.com/channel/UCI7G6fNN-17g1_tOVMKRCpQ" target="_blank" class="reseauSociaux" id="youtube"><img src="assets/img/logo youtube.png" alt="youtube"></a>
+                <a href="https://discord.gg/starisland" target="_blank" class="reseauSociaux discord"><img src="assets/img/logo discord.png" alt="discord"></a>
             </div>
 
         </div>
@@ -52,10 +64,8 @@
             tRebour = setTimeout("Rebour();", 1000);
         }
         Rebour();
-
-        
     </script>
-    
+
 
 </body>
 
