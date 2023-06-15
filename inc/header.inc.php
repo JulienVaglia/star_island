b18d23d6252661c9b807362154980275fbabf3de
JulienVaglia <julien.vaglia@gmail.com>
2023-06-15 16:46:48 +0200

    first_index

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,72 +1,69 @@
-
 <!doctype html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style_nav.css">  
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.2.3/lux/bootstrap.min.css" integrity="sha512-+TCHrZDlJaieLxYGAxpR5QgMae/jFXNkrc6sxxYsIVuo/28nknKtf9Qv+J2PqqPXj0vtZo9AKW/SMWXe8i/o6w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <link rel="stylesheet" href="assets/css/style-index.css">
 </head>
+
 <body>
 
-<header>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-5">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="<?=  BASE_PATH; ?>">CRUD</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-5">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="<?= BASE_PATH; ?>">
+                    <img id="logo" src="assets/img/starisland.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarColor01">
-            <ul class="navbar-nav me-auto">
-                <li class="nav-item">
-                    <a class="nav-link active" href="<?=  BASE_PATH; ?>">Home
-                        <span class="visually-hidden">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <?php     if (admin()):           ?>
-                <li class="nav-item dropdown">
+                <div class="collapse navbar-collapse" id="navbarColor01">
+                    <ul class="navbar-nav me-auto">
+                        <li>
+                            <a class="nav-link active" href="<?= BASE_PATH; ?>"><img id="logo_acceuil" src="assets/icon/accueil.png" alt="Acceuil">
+                                <span class="visually-hidden">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">GALLERIE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">VIP</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">SERVEUR</a>
+                        </li>
+                        <?php if (admin()) :           ?>
+                            <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">ADMIN</a>
                     <div class="dropdown-menu">
-                        <a class="dropdown-item" href="<?=  BASE_PATH.'back/userList.php'; ?>">Gestion utilisateur</a>
+                        <a class="dropdown-item" href="<?= BASE_PATH . 'back/userList.php'; ?>">Gestion utilisateur</a>
                         <a class="dropdown-item" href="#">Another action</a>
                         <a class="dropdown-item" href="#">Something else here</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="<?=  BASE_PATH.'back/'; ?>">Accès Back-office</a>
+                        <a class="dropdown-item" href="<?= BASE_PATH . 'back/'; ?>">Accès Back-office</a>
                     </div>
                 </li>
-                <?php     endif;           ?>
+                        <?php endif;           ?>
 
-            </ul>
-            <?php     if (connect()):           ?>
-            <a href="<?=  BASE_PATH.'?a=dis'; ?>" class="btn btn-primary">Déconnexion</a>
-            <?php     else:           ?>
-            <a href="<?=  BASE_PATH.'security/login.php'; ?>" class="btn btn-primary">Connexion</a>
-            <a href="<?=  BASE_PATH.'security/register.php'; ?>" class="btn btn-success">Inscription</a>
-            <?php        endif;        ?>
+                    </ul>
+                    <div class="right_nav">
+                            <a href="<?= BASE_PATH . 'security/login.php'; ?>" ><img src="assets/icon/tableau-daffichage.png" alt=""><br> TUTORIEL</a>
+                            <a href="<?= BASE_PATH . 'security/register.php'; ?>" class=><img src="assets/icon/bulle-de-discussion.png" alt=""><br>EVENT</a>
+                        </div>
 
-        </div>
-    </div>
-</nav>
-</header>
-<main class="container">
-    <?php     if (isset($_SESSION['messages']) && !empty($_SESSION['messages'])):           ?>
-    <?php     foreach ($_SESSION['messages'] as $type=>$messages):
-      ?>
-    <?php     foreach ($messages as $key=>$message):           ?>
-    <div class="alert alert-<?=  $type; ?> text-center w-50 mx-auto">
-        <p><?=  $message; ?></p>
-    </div>
+                    <?php if (connect()) :           ?>
+                        <a href="<?= BASE_PATH . '?a=dis'; ?>" class="right_nav">Déconnexion</a>
+                    <?php else :           ?>
+                    
+                    <?php endif;        ?>
 
-    <?php   unset($_SESSION['messages'][$type][$key]);
-            endforeach;  endforeach;  endif;           ?>
+                </div>
+            </div>
+        </nav>
+    </header> 
--- index.php
+++ index.php
@@ -1,19 +1,71 @@
-<?php      require_once 'config/function.php';
-            require_once 'inc/header.inc.php';
-
-            if (isset($_GET['a']) && $_GET['a']=='dis'){
-
-                unset($_SESSION['user']);
-                $_SESSION['messages']['info'][]='A bientôt !!';
-                header('location:./');
-                exit();
+<?php require_once 'config/function.php';
+require_once 'inc/header.inc.php';
 
 
-            }
-            ?>
+?>
 
+<main>
 
+    <h1>Bienvenue sur Star'Island</h1>
 
+    <!-- --------- Carroussel principal --------- -->
 
+    <div class="slider">
+        <input type="radio" name="toggle" id="btn-1" checked>
+        <input type="radio" name="toggle" id="btn-2">
+        <input type="radio" name="toggle" id="btn-3">
 
-<?php     require_once 'inc/footer.inc.php';          ?>
+        <div class="slider-controls">
+            <label for="btn-1"></label>
+            <label for="btn-2"></label>
+            <label for="btn-3"></label>
+        </div>
+
+        <ul class="slides">
+
+            <!-- Slide 1 Texte -->
+
+            <li class="slide">
+                <p class="slide-image">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et temporibus doloremque, culpa illum quibusdam eum eaque in voluptatibus aperiam blanditiis magni ea atque qui! A culpa corporis fuga eum ipsam.
+                    Fugit enim soluta sunt magnam omnis, possimus, porro autem facere necessitatibus distinctio consequuntur impedit, quam laborum hic fugiat mollitia! Distinctio corporis laboriosam velit cum quis labore cupiditate praesentium, tempora ipsam?
+                </p>
+            </li>
+
+            <!-- Slide 2 Carroussel -->
+
+            <li class="slide">
+                <p class="slide-image">
+                <div class="container2">
+                    <input type="radio" name="slider" id="item-1" checked>
+                    <input type="radio" name="slider" id="item-2">
+                    <input type="radio" name="slider" id="item-3">
+                    <div class="cards">
+                        <label class="card" for="item-1" id="song-1">
+                            <img src="https://images.unsplash.com/photo-1530651788726-1dbf58eeef1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=882&q=80" alt="song">
+                        </label>
+                        <label class="card" for="item-2" id="song-2">
+                            <img src="https://images.unsplash.com/photo-1559386484-97dfc0e15539?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80" alt="song">
+                        </label>
+                        <label class="card" for="item-3" id="song-3">
+                            <img src="https://images.unsplash.com/photo-1533461502717-83546f485d24?ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" alt="song">
+                        </label>
+                    </div>
+
+                </div>
+                </p>
+            </li>
+
+            <!-- Slide 3 API Top serveur -->
+
+            <li class="slide">
+                <p class="slide-image">
+                    3 - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa, quam consequuntur voluptas ut non illum aut! Sapiente fugit enim ipsa dolor quibusdam, maxime voluptatum molestiae deleniti ipsam. Enim, vel qui.
+                    Corrupti repellat sequi debitis, distinctio neque veritatis ex. Iure molestias officiis rem cupiditate repellat? Officiis ad molestias, dolore delectus aliquid laborum veniam! Ipsam reprehenderit sequi suscipit magnam, consequatur nulla tempore.
+                </p>
+            </li>
+        </ul>
+    </div>
+
+</main>
+</div <?php require_once 'inc/footer.inc.php';          ?>
