988d0a1cb0658c367d13595ed7f319d8671094eb
JulienVaglia <julien.vaglia@gmail.com>
2023-06-16 12:33:54 +0200

    plouf

--- teaser.php
+++ teaser.php
@@ -36,10 +36,11 @@
                 <a href="https://www.instagram.com/starisland.fr/" target="_blank" class="reseauSociaux" id="instagram"><img src="assets/img/logo Instagram.png" alt="instagram"></a>
                 <a href="https://www.facebook.com/StarIslandfr-108004258577047" target="_blank" class="reseauSociaux" id="facebook"><img src="assets/img/logo facebook.png" alt="facebook"></a>
                 <a href="https://www.tiktok.com/@star.island?lang=fr" target="_blank" class="reseauSociaux" id="tiktok"><img src="assets/img/logo tiktok.png" alt="tiktok"></a>
+                  <a href="https://discord.gg/starisland" target="_blank" class="reseauSociaux discord"><img src="assets/img/logo discord.png" alt="discord"></a>
                 <a href="" class="reseauSociaux" target="_blank" id="twitch"><img src="assets/img/logo twitch.png" alt="twitch"></a>
                 <a href="https://twitter.com/StarIslandfr" target="_blank" class="reseauSociaux" id="twitter"><img src="assets/img/logo twitter.png" alt="twitter"></a>
                 <a href="https://www.youtube.com/channel/UCI7G6fNN-17g1_tOVMKRCpQ" target="_blank" class="reseauSociaux" id="youtube"><img src="assets/img/logo youtube.png" alt="youtube"></a>
-                <a href="https://discord.gg/starisland" target="_blank" class="reseauSociaux discord"><img src="assets/img/logo discord.png" alt="discord"></a>
+              
             </div>
 
         </div>
