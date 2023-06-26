8892165807595e70af32b129046f8f76876b3ad5
JulienVaglia <julien.vaglia@gmail.com>
2023-06-26 15:29:06 +0200

    start serveur

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -39,7 +39,7 @@
                             <a class="nav-link" href="vip.php">DEVENIR VIP</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">SERVEUR</a>
+                            <a class="nav-link" href="serveur.php">SERVEUR</a>
                         </li>
                         <!-- <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
---
+++ serveur.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'config/function.php';
+require_once 'inc/header.inc.php';
+?>
+
+<main class="main_serveur">
+    <link rel="stylesheet" href="assets/css/style_serveur.scss">
+
+
+
+
+
+
+
+
+    <?php
+    require_once 'inc/barre_lien.inc.php';
+    ?>
+
+</main>
+
+<?php require_once 'inc/footer.inc.php'; ?>
